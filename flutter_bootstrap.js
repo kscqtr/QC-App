942d52a896531f3c73aaee4499b912c381c67f82
kscqtr <qtr@keystone-cable.com>
2025-05-28 12:53:08 +0800

    25.05.28: Completed Page 2 of IEC60332-3-22/24

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1701690683"
+    serviceWorkerVersion: "3254480041"
   }
 });
